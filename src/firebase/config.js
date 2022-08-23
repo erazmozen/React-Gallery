0e6101ba958d05090253be4d86117db967590eb2
Erazmo Zen <67731959+erazmozen@users.noreply.github.com>
2022-08-23 11:16:47 +0200

    Initial setup

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqoVDEOO0IEurHxiW9N0nGoQXoEjI7tlE",
+  authDomain: "foodfest-a1c45.firebaseapp.com",
+  projectId: "foodfest-a1c45",
+  storageBucket: "foodfest-a1c45.appspot.com",
+  messagingSenderId: "380594955966",
+  appId: "1:380594955966:web:46124ae3298ecf125360ef",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
