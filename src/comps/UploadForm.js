baa25a7106bc117f52a662a91d950d7c17e72f28
Erazmo Zen <67731959+erazmozen@users.noreply.github.com>
2022-08-23 11:36:50 +0200

    Filepicker logic

---
+++ src/comps/UploadForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const UploadForm = () => {
+  // State u kome cuvamo izabrani file
+  const [file, setFile] = useState(null);
+  // Dozvoljeni filetype
+  const types = ["image/png", "image/jpeg"];
+  // Error
+  const [error, setError] = useState(null);
+
+  const changeHandler = (e) => {
+    // Iz event objekta uzimamo samo prvi file, stavicemo ga u local state
+    let selected = e.target.files[0];
+    // Samo ako je file izabran koristimo setFile da bismo update state. Takodje
+    // moramo da proverimo dali je file type ono sto zelimo, napravicemo array sa
+    // dozvoljenim filetype, definisano gore
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError("");
+    } else {
+      setFile(null);
+      setError("File mora biti png ili jpeg");
+    }
+  };
+
+  return (
+    <form>
+      <input type="file" onChange={changeHandler} />
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+        {file && <div>{file.name}</div>}
+      </div>
+    </form>
+  );
+};
+
+export default UploadForm;
